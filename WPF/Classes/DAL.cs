fb59a62ada3678c8d3f36087d787fdc393375ba8
Mohammed Amine Boumediene Blal <darkous@hotmail.com>
2025-02-14 08:25:45 -0500

    [feat] Connection base de données

--- WPF/Classes/DAL.cs
+++ WPF/Classes/DAL.cs
@@ -1,13 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using MySqlConnector;
+using System.IO;
 
 namespace AppRobot.Classes
 {
-    static class DAL
+    public static class DAL
     {
+        public const string APPSETTING_FILE = "appsettings.json";
+
+        private const string CONNECTION_STRING = "DefaultConnection";
+
+        public const string PRODUIT_IMAGES = "Images:Path";
+
+        private static IConfiguration _configuration;
+
+        /// <summary>
+        /// Constructeur static permettant de charger les configurations de l'application
+        /// </summary>
+        static DAL()
+        {
+            _configuration = new ConfigurationBuilder().AddJsonFile(APPSETTING_FILE, false, true).Build();
+        }
+
+        public static MySqlConnection Connection()
+        {
+            return new MySqlConnection(_configuration.GetConnectionString(CONNECTION_STRING));
+        }
+
 
     }
 }
