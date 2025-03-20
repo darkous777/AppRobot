256211870689ce27725e699215c48719e471ac32
Vincent Rompré <150175646+AtomGjectas@users.noreply.github.com>
2025-03-20 17:50:45 -0400

    Feat : Ajout des 4 boutons principales dans l'interface de controle du robot : Forward, Backward, RotationRight et RotationLeft

--- WPF/Views/Robot.xaml.cs
+++ WPF/Views/Robot.xaml.cs
@@ -140,5 +140,27 @@ namespace AppRobot.Views
                 _pressedKeys.Remove(e.Key);
             }
         }
+
+        private void btnForward_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        
+
+        private void btnBackward_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRotationRight_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnRotationLeft_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
