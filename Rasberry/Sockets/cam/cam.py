b6767e8758b865864bcfcaa3d8f65e441f35ca4e
Amine Blal <darkous@hotmail.com>
2025-04-03 19:48:37 -0400

    [fix] url fonctionnement entre les deux ordi

--- Rasberry/Sockets/cam/cam.py
+++ Rasberry/Sockets/cam/cam.py
@@ -1,12 +1,19 @@
 import cv2
 from threading import Thread
 import time
-import numpy as np
+
 
 class WebcamVideoStream:
-    def __init__(self, src = 0):
+    def __init__(self, src = 0,width = 640,height = 480):
         print("init")
         self.stream = cv2.VideoCapture(src)
+
+        self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+
+        self.stream.set(3,width)
+        self.stream.set(4,height)
+
+
         (self.grabbed, self.frame) = self.stream.read()
         self.stopped = False
         time.sleep(2.0)
--- Rasberry/Sockets/cam/flask_cam.py
+++ Rasberry/Sockets/cam/flask_cam.py
@@ -1,10 +1,11 @@
 import threading
 import cv2
-from flask import Flask, Response
+from flask import Flask, Response, render_template
 from cam import WebcamVideoStream
 from flask_basicauth import BasicAuth
 import os
 import signal
+import time
 
 app = Flask(__name__)
 app.config['BASIC_AUTH_USERNAME'] = 'pi'
@@ -16,7 +17,11 @@ active_connections = 0
 camera = None  # Instance unique de la caméra
 lock = threading.Lock()  # Sécuriser l'accès au compteur
 
-
+@app.route('/')
+@basic_auth.required
+def index():
+    return render_template('index.html')
+ 
 def gen():
     global active_connections, camera
     with lock:
@@ -30,12 +35,18 @@ def gen():
             if camera.stopped:
                 break
             frame = camera.read()
+
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            frame = cv2.resize(frame, (640,480))
             ret, jpeg = cv2.imencode('.jpg', frame)
-            if jpeg is not None:
+            if ret:
                 yield (b'--frame\r\n'
                        b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
             else:
                 print("frame is none")
+
+            time.sleep(0.05)
     except GeneratorExit:
         pass  # L'utilisateur a quitté
 
@@ -53,7 +64,7 @@ def shutdown():
     return "Server shutting down..."
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, threaded=True)
+    app.run(host='0.0.0.0',port=8080, debug=True, threaded=True)
 
 
 # http://127.0.0.1:5000/video_feed
