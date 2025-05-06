0e3699f62f616bab6f49100448319a9d54305d51
Mohammed Amine Boumediene Blal <darkous@hotmail.com>
2025-05-05 22:28:24 -0400

    [feat] script creation de la bd

---
+++ AppRobot/LibrairieInstallation/PostInstall.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Diagnostics;
+using System.IO;
+
+namespace LibrairieInstallation
+{
+    [RunInstaller(true)]
+    public class PostInstall : Installer
+    {
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+
+            string targetDir = Context.Parameters["targetdir"];
+            string exePath = Path.Combine(targetDir, "InitialisationBD.exe");
+
+            if (File.Exists(exePath))
+                Process.Start(exePath);
+        }
+    }
+}
